024030cfb16f82d2e0a5ea8d02913bc8e7ac96d5
Kenny Chan <kennych418@ucla.edu>
2019-10-17 14:16:05 -0700

    Adding Week 2

--- Week2Client/clienttest.py
+++ Week2Client/clienttest.py
@@ -1,7 +1,7 @@
 import socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('107.77.227.199', 8080))
-client.send("I am CLIENT\n")
-from_server = client.recv(4096)
+client.connect(('172.20.10.3', 8080))
+client.send("I am CLIENT\n".encode())
+from_server = client.recv(4096).decode()
 client.close()
 print(from_server)
