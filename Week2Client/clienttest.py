63258470abd5f0a8e790465ba3717040a579445e
Kenny Chan <kennych418@ucla.edu>
2019-11-22 17:41:22 -0800

    Adding weekly reports

--- Week2Client/clienttest.py
+++ Week2Client/clienttest.py
@@ -1,7 +1,9 @@
 import socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('172.20.10.3', 8080))
+client.connect(('192.168.0.130', 8080))
 client.send("I am CLIENT\n".encode())
 from_server = client.recv(4096).decode()
+while(1):
+    client.send("I am CLIENT\n".encode())
 client.close()
 print(from_server)
